f0a75a5200f2c604833a1d1ffa99fb1b722814b7
sophiaristuben <sophiaristuben@gmail.com>
2023-12-01 17:09:46 -0800

    new burger sprites

--- src/sprites.rs
+++ src/sprites.rs
@@ -27,13 +27,13 @@ pub const ball_size: f32 =CELL_WIDTH - 20.0 as f32;
 
 pub fn create_sprites() -> Vec<GPUSprite> {
     let mut sprites: Vec<GPUSprite> = vec![
-        GPUSprite { //0 somethin weird is happenign where 1-4 are not showing up
-            screen_region: [WINDOW_WIDTH/2.0, -16.0, ball_size, ball_size],
-            sheet_region: [0.5, 2.0 / 3.0, 0.5, 0.5], // orange ball (platformer)
+        GPUSprite { //0 somethin weird is happenign where 1-4 are not showing up 
+            screen_region: [WINDOW_WIDTH/2.0, -16.0, 128.0, 128.0],
+            sheet_region: [0.0,0.0, 0.2, 0.2], // orange ball (platformer)
         },
         GPUSprite { //1
             screen_region: [128.0, 500.0, ball_size, ball_size],
-            sheet_region: [0.5, 1.0/3.0, 0.5, 0.35], // yellow ball
+            sheet_region: [0.0,0.0, 0.2, 0.2], // yellow ball
         },
     ];
 
@@ -41,50 +41,25 @@ pub fn create_sprites() -> Vec<GPUSprite> {
         for col in 0..(NUMBER_OF_CELLS+1) as usize {
             let x_position = col as f32 * CELL_WIDTH;
             let y_position = WINDOW_HEIGHT - (row as f32 * CELL_HEIGHT);
-    
-            // Use modulo to cycle through all four colors
-            let color_region = match (row % 2, col % 2) {
-                (0, 0) => [0.0, 0.0, 0.5, 0.335], // pink ball
-                (0, 1) => [0.0,  1.0/3.0, 0.5, 0.335], // green ball
-                (1, 0) => [0.5, 1.0/3.0, 0.5, 0.335], // Green brick
-                (1, 1) => [0.0, 2.0 / 3.0, 0.5, 0.335], // Yellow brick
-                _ => unreachable!(),
-            };
-    
+
+            let top_bun =[0.0,0.0, 0.2, 0.05];
+            let bottom_bun= [0.0, 0.05, 0.2, 0.04];
+            let cheese = [0.0, 0.1, 0.2, 0.04];
+            let meat = [0.0, 0.14, 0.2, 0.04];
+            let sauce = [0.0, 0.18, 0.2, 0.05];
+            let lettuce = [0.0, 0.23, 0.2, 0.05];
+
+
             sprites.push(GPUSprite {
-                screen_region: [x_position, y_position, ball_size, ball_size],
-                sheet_region: color_region,
+                screen_region: [x_position, y_position, 64.0, 16.0],
+                sheet_region: [0.0, 0.23, 0.2, 0.05],
             });
         }
     }
     
     
+
     
-
-    // for row in 0..5 {
-    //     for col in 0..(WINDOW_WIDTH / CELL_WIDTH) as usize {
-    //         let x_position = col as f32 * CELL_WIDTH;
-    //         let y_position = WINDOW_HEIGHT - (row as f32 * CELL_HEIGHT);
-
-    //         sprites.push(GPUSprite {
-    //             screen_region: [x_position, y_position, CELL_WIDTH, CELL_HEIGHT],
-    //             sheet_region: [0.0, 1.0 / 3.0, 0.5, 0.5 / 3.0], // blue brick
-    //         });
-    //         sprites.push(GPUSprite {
-    //             screen_region: [x_position, y_position, CELL_WIDTH, CELL_HEIGHT],
-    //             sheet_region: [0.0, 0.0, 0.5, 0.5 / 3.0], // pink brick
-    //         });
-    //         sprites.push(GPUSprite {
-    //             screen_region: [x_position, y_position, CELL_WIDTH, CELL_HEIGHT],
-    //             sheet_region: [0.5, 1.0 / 3.0, 0.5, 0.5 / 3.0], // purple brick
-    //         });
-    //         sprites.push(GPUSprite {
-    //             screen_region: [x_position, y_position, CELL_WIDTH, CELL_HEIGHT],
-    //             sheet_region: [0.0, 2.0 / 3.0, 0.5, 0.5 / 3.0], // yellow brick
-    //         });
-    //     }
-    // };
-
     sprites
 }
 
@@ -94,11 +69,11 @@ pub fn create_sprites() -> Vec<GPUSprite> {
 pub fn move_platform(input: &Input, mut platform_position: [f32; 2]) -> [f32; 2] {
     if input.is_key_down(winit::event::VirtualKeyCode::Left) {
         platform_position[0] -= 5.0;
-        // change platform angle
+// change platform angle
     }
     if input.is_key_down(winit::event::VirtualKeyCode::Right) {
         platform_position[0] += 5.0;
-        // change platform angle
+// change platform angle
     }  
     if input.is_key_pressed(winit::event::VirtualKeyCode::Space) {
         // shoot ball
